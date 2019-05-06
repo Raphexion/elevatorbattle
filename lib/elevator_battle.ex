c804802a837d476c288ec1146693a02cc13ce0e2
Niklas Johansson <raphexion@gmail.com>
2019-05-06 17:45:14 +0200

    Initial commit

---
+++ lib/elevator_battle.ex
@@ -0,0 +1,18 @@
+defmodule ElevatorBattle do
+  @moduledoc """
+  Documentation for ElevatorBattle.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElevatorBattle.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elevator_battle_test.exs
@@ -0,0 +1,8 @@
+defmodule ElevatorBattleTest do
+  use ExUnit.Case
+  doctest ElevatorBattle
+
+  test "greets the world" do
+    assert ElevatorBattle.hello() == :world
+  end
+end
